3d8a4c62af6ecf4996a83b3d148a851957af4462
Maria Manninen <maria.vihla@gmail.com>
2021-01-19 10:30:20 +0100

    added first validations for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  # validates :instruction, presence: true, length: { minimum: 10 }
 end
