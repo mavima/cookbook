53cb052c5a6cbdf4eafbe2e2a05bec5fe9eee891
Maria Manninen <maria.vihla@gmail.com>
2021-01-19 18:41:00 +0100

    migrated ingredient

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_101932) do
+ActiveRecord::Schema.define(version: 2021_01_19_173001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.float "amount"
+    t.string "unit"
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -43,5 +53,6 @@ ActiveRecord::Schema.define(version: 2021_01_19_101932) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ingredients", "recipes"
   add_foreign_key "recipes", "users"
 end
