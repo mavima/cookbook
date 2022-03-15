9a0a6af180e8745d5d34248f6aeb0a9886b8e0ae
Maria Manninen <maria.vihla@gmail.com>
2022-03-15 11:12:25 +0100

    reverting again

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,4 +7,5 @@ class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :admin, :avatar)
   end
-end
+end 
+
