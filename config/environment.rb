45840115f8ebdb6f4ab6e28e862ea532b2a59184
Maria Manninen <maria.vihla@gmail.com>
2022-03-16 11:44:38 +0100

    actionMailer fix

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 # config.action_mailer.default_url_options = { :host => 'localhost' }
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'info@recipeper.com', 
+  :user_name => 'apikey', 
   :password => ENV["SENDGRID_API_KEY"], # This is the secret sendgrid API key which was issued during API key creation
   :domain => 'https://recipepers.herokuapp.com',
   :address => 'smtp.sendgrid.net',
