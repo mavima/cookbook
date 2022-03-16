9643b4f2cde39a75bc4d00d395766bf7498c333f
Maria Manninen <maria.vihla@gmail.com>
2022-03-16 10:37:36 +0100

    fixing sendgrid in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,7 +107,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-
+  config.action_mailer.delivery_method = :smtp
   # UNCOMMENT
   config.action_mailer.default_url_options = { host: 'https://recipepers.herokuapp.com' }
   config.active_storage.service = :cloudinary
