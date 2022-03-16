60c70d16173d0afc08e38181900ed887fba518d9
Maria Manninen <maria.vihla@gmail.com>
2022-03-16 14:50:34 +0100

    recipe name on email subject

--- app/mailers/recipe_mailer.rb
+++ app/mailers/recipe_mailer.rb
@@ -3,7 +3,8 @@ class RecipeMailer < ApplicationMailer
     def send_recipe(user, recipe)
         @user = user
         @recipe = recipe
+        @subject = 'Recipeper: ' + @recipe.name
         headers['recipe'] = {"id": @recipe.id}
-        mail(to: user.email, subject: 'Recipe from Recipeper')
+        mail(to: user.email, subject: @subject )
     end
 end
