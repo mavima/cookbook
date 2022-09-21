01b5527c7f68ab5288b9c43d3809bf31eaaab5cb
Maria Manninen <maria.vihla@gmail.com>
2022-09-21 14:59:40 +0200

    fixed delete confimations for review and link

--- app/javascript/functions/modal_toggle.js
+++ app/javascript/functions/modal_toggle.js
@@ -8,9 +8,21 @@ const modalToggle = () => {
     $(this).next('.comment-modal').show()
   })
 
+  $('.delete-review-click').click(function() {
+    $(this).next('.delete-confirmation-modal').show()
+  })
+
+  $('.delete-link-click').click(function() {
+    console.log('click')
+    console.log(this)
+    $(this).next('.delete-link-confirmation-modal').show()
+  })
+
+
   $('.close-modal-btn').on('click', (e) => {
     $('.show-mail-modal').hide()
     $('.comment-modal').hide()
+    $('.delete-confirmation-modal').hide()
   })
 
 
