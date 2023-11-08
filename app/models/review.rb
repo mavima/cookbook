5a8eaa082ca511ccc6acced257832fa9d245346e
mavima <maria.vihla@gmail.com>
2023-11-08 18:02:11 +0100

    added sanitazion and validation preventing code inject

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,8 @@ class Review < ApplicationRecord
 
   mount_uploader :photo, PhotoUploader
 
-  validates :content, presence: true
+
+  validates :content, presence: true, format: { with: /\A[a-zA-Z0-9.:,;?!'"&\s]*\z/, message: ": no special characters allowed" }
   validates :rating, presence: true
   validates :writer, presence: true, if: :guest_user?
 
